a98f68b3309da5b478fa8b79f2486e14eb1e0cc0
Aaditya <aadityathapa977@gmail.com>
2024-12-07 17:10:54 +0545

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+const Home = () => {
+  return <div className="bg-primary h-full w-full"></div>;
+};
+
+export default Home;
