b455e6f7c1c7ac0c5364de10a5f401ca63fd5f32
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2024-12-07 20:02:18 +0545

    Complete login and register page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,22 @@
+import ThirdParty from "@/components/ThirdParty";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
-    <>
-      <div className="container mx-3 grid md:grid-cols-2 gap-10">
-        <div className="image-section justify-self-end hidden md:block">
-            <img className="h-4/5" src="https://i.pinimg.com/736x/46/fe/c0/46fec04a273ccc1a380cb882615fafbd.jpg" alt="IMG" />
-        </div>
-        <div className="login-page-section">
+    <div className="grid h-full w-full gap-10 px-3 md:grid-cols-2">
+      <div className="image-section hidden self-center justify-self-end md:block">
+        <Image
+          className="rounded-3xl"
+          src="https://media.discordapp.net/attachments/1275803991804084258/1314939394423128135/46fec04a273ccc1a380cb882615fafbd.png?ex=6755983e&is=675446be&hm=4d17bfbe3192d889f43207f465a777eb7a654f68f2a9058ee8313fb7b9dbdfba&=&format=webp&quality=lossless&width=351&height=701"
+          width={500}
+          height={500}
+          alt="Image of an astronaut"
+        />
+      </div>
+      <div className="login-page-section md:justify-self-start">
         <div className="brand-section my-10 flex items-center">
           <img
             className="w-20"
@@ -19,40 +26,46 @@ const page = () => {
           <div className="brand-name font-poppins">BlogSpot</div>
         </div>
         <div className="mb-5 font-bold">Nice to see you again</div>
-        <form className="space-y-5" action="">
+        <form className="space-y-3" action="">
           <div className="email-section">
-            <label className="ml-5 text-sm text-gray-500" htmlFor="">
-              Login
+            <label className="ml-3 text-sm text-gray-500" htmlFor="">
+              Email
             </label>
             <Input
-              className="bg-gray-200 text-sm"
+              className="focus mt-1 bg-gray-200 text-sm"
               type="email"
-              placeholder="Email..."
+              // placeholder="Enter your email"
             />
           </div>
           <div className="password-section">
-            <label className="ml-5 text-sm text-gray-500" htmlFor="">
+            <label className="ml-3 text-sm text-gray-500" htmlFor="">
               Password
             </label>
             <Input
-              className="bg-gray-200 text-sm"
+              className="mt-1 bg-gray-200 text-sm"
               type="email"
-              placeholder="Password..."
+              // placeholder="Password"
             />
           </div>
           <div className="memory flex justify-between">
             <div className="remember flex items-center gap-2">
-            <Checkbox />
-            <p className="text-sm">Remember Me</p>
+              <Checkbox />
+              <p className="text-sm">Remember Me</p>
             </div>
             <div className="forgot">
-                <p className="text-blue-300 text-sm">Forgot Password?</p>
+              <p className="cursor-pointer text-sm text-blue-400">
+                Forgot Password?
+              </p>
             </div>
           </div>
         </form>
-        </div>
+        <ThirdParty content="Sign in" />
+        <p className="mt-20 text-center text-sm">
+          Don't have an account?{" "}
+          <span className="cursor-pointer text-blue-400">Sign up now</span>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/ThirdParty.jsx
+++ src/components/ThirdParty.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { FaGoogle, FaGithub } from "react-icons/fa";
 
-const ThirdParty = () => {
+const ThirdParty = (props) => {
   return (
     <div>
-      <div className="third-party-signup">
-        <button className="mb-4 mt-2 flex w-full items-center justify-center gap-4 rounded-sm border px-2 py-3 lg:w-10">
+      <div className="third-party-signup grid md:justify-center mt-10">
+        <button className="mb-4 mt-2 flex w-full items-center justify-center gap-4 rounded-lg border px-2 py-3 lg:w-96 text-sm hover:bg-black hover:text-white transition-colors duration-300">
           <FaGoogle />
-          Sign Up With Google
+          {props.content} With Google
         </button>
-        <button className="flex w-full items-center justify-center gap-4 rounded-sm border px-2 py-3 lg:w-10">
+        <button className="flex w-full items-center justify-center gap-4 rounded-lg border px-2 py-3 lg:w-96 text-sm hover:bg-black hover:text-white transition-colors duration-300">
           <FaGithub />
-          Sign Up With GitHUb
+          {props.content} With GitHUb
         </button>
       </div>
     </div>
