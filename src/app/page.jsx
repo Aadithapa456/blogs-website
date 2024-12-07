182c3a17fdd970a82e707494b28c2085dcece432
Aaditya <aadityathapa977@gmail.com>
2024-12-07 17:31:18 +0545

    Add login form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import Home from "@/components/Home";
+import Login from "@/components/Login";
 import Image from "next/image";
 export default function Page() {
   return (
     <>
-      <Home />
+      <Login />
     </>
   );
 }
