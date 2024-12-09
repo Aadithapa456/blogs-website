2adc754c07f274cbb9d817fffe121e81cef3c933
Aaditya <aadityathapa977@gmail.com>
2024-12-09 22:25:31 +0545

    feat: Improve hero cards

--- src/components/CardLandscape.jsx
+++ src/components/CardLandscape.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Cardlandscape = () => {
-  return <div className="mx-auto mb-4 h-32 w-3/4 bg-gray-400 md:w-40"></div>;
+  return (
+    <div className="mx-auto mb-4 h-44 w-2/3 bg-gray-400 md:h-40 md:w-52 lg:h-44 lg:w-72"></div>
+  );
 };
 
 export default Cardlandscape;
