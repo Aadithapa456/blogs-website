1ce33612a9266d63f9e7870739ba0970b3e2a348
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2024-12-10 14:50:09 +0545

    Fix grid pattern in home

--- src/components/CardLandscape.jsx
+++ src/components/CardLandscape.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cardlandscape = (props) => {
   return (
-    <div className="box flex h-32 w-64 items-center justify-center border-2 border-black bg-slate-800 text-2xl font-bold text-white">
+    <div className="box flex h-32 w-96 items-center justify-center border-2 border-black bg-slate-800 text-2xl font-bold text-white rounded-[10px]">
         {props.title}
       </div>
   );
